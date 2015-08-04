d3373459605f1205fc4cbe62b86a1d674f194992
jonnyspringare <jonny.springare@lundalogik.se>
2015-08-04 12:57:25 +0200

    Update csp_easytopro_validate_field.sql

--- easytopro/Install/SQL/csp_easytopro_validate_field.sql
+++ easytopro/Install/SQL/csp_easytopro_validate_field.sql
@@ -88,7 +88,10 @@ AS
                                                     AND e2.[protable] = e.[protable]
                                                     AND e2.[idfieldmapping] != e.[idfieldmapping]
                                                     AND e2.[active] = 1
-                                                    AND l.[fieldname] IS NOT NULL )
+                                                    AND l.[fieldname] IS NOT NULL
+                                                    AND e.[easyfieldtype] <> 6
+                                                    AND e.[easyfieldtype] <> 7 
+                                                    AND e.[easyfieldtype] = e2.[easyfieldtype])
                              THEN REPLACE(REPLACE(N'LIME Pro table ''#1#'', field ''#2#'': Existing Pro Field mapped more than once.',
                                                   N'#1#', e.[protable]),
                                           N'#2#', e.[profieldname])
@@ -116,7 +119,7 @@ AS
                                               + e.[localname_no]
                                               + e.[localname_fi]
                                               + e.[localname_da]) > 0 )
-                             THEN REPLACE(REPLACE(N'LIME Pro table ''#1#'', field ''#2#'': Local name can´t contain character ''.''',
+                             THEN REPLACE(REPLACE(N'LIME Pro table ''#1#'', field ''#2#'': Local name canÂ´t contain character ''.''',
                                                   N'#1#', e.[protable]),
                                           N'#2#', e.[profieldname])
                              ELSE N''
